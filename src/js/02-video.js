938ff84834fe7d2ec2586ac99f5e29466ef5dc1a
Makar2006 <makar.diakiv@gmail.com>
2023-08-04 18:59:27 +0200

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const LOCAL_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(data.seconds));
+  }),
+  1000
+);
+
+const currentTime = localStorage.getItem(LOCAL_KEY) || 0;
+player.setCurrentTime(currentTime);
